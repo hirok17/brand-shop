48c6d9217dcaeaac6a513357c37ec2b9587010bc
hirok17 <hirokray17@gmail.com>
2023-10-22 16:23:51 +0600

    category page design

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -8,31 +8,24 @@ const Products = () => {
     const data = useLoaderData();
     console.log(data);
     return (
-       <section>
+       <section className="mb-9">
             <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src={data.slideOne} className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                        <a href="#slide4" className="btn btn-circle">❮</a>
-                        <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                    <img src="https://i.ibb.co/znwjktT/sneaker-that-has-word-nike-it.jpg" className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                    <img src="https://i.ibb.co/5kH2R6S/hnslide.jpg" className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide4" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide4" className="carousel-item relative w-full">
-                    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                    <img src="https://i.ibb.co/6w0prv4/hq720.jpg" className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
@@ -41,7 +34,7 @@ const Products = () => {
             </div>
 
             {/* slider aera end */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-9">
             {
                 data.map(brand=> <Brand key={brand._id} brand={brand}></Brand>)
             }
